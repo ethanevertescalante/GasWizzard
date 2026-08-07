0abe853d2a7cc2f27ad847fd24d7c4cb6410a08d
ethanescalante <ethan65g@gmail.com>
2026-08-06 20:29:38 -0700

    made /map route the new permanent homepage

--- apps/GasWizzard/next.config.js
+++ apps/GasWizzard/next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     allowedDevOrigins: ["huntsman-reenter-giggling.ngrok-free.dev"],
+    async redirects() {
+        return [
+            {
+                source: '/',
+                destination: '/map',
+                permanent: true,
+            }
+        ]
+    }
 };
 
 export default nextConfig;
